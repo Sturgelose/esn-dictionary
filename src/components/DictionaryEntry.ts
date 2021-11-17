ace52eea1aec0adfe9cb39fdd17df3e675a03bf4
Erik Berdonces <sturgelose@gmail.com>
2021-11-17 16:13:40 +0100

    Updates

--- src/components/DictionaryEntry.ts
+++
@@ -1,9 +0,0 @@
-export interface DictionaryEntry {
-    term1: string
-    term2: string
-    grammar: string
-    definition: string
-    example: string
-    category: string
-    anchor: string
-}
